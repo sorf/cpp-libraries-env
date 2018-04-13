272b6599375e5355862e4b77f80b2b7abd13e129
Sorin Fetche <sorin.fetche@gmail.com>
2018-04-13 03:51:59 -0700

    Boost 1.66. Plus: - use default path for MinGW - upgrade clang script to 5.0

---
+++ clang_5.0_release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export CLANGXX_COMMAND="clang++-5.0"
+export CLANG_COMMAND="clang-5.0"
+# We need gcc as well for Boost.Build.
+export GCC_COMMAND="/usr/bin/gcc"
+export PS1="[clang_5.0-release]\u@\h:\w>"
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${script_dir}/env_linux.sh clang_5.0 clang release && exec bash
+
